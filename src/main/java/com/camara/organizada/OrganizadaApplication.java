59f7c05ecc6cba05bb69bc2a1485c0bb95a83ad6
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2019-10-15 14:43:12 -0300

    Adicionando cache nas requisiçoes de rulling partie

--- src/main/java/com/camara/organizada/OrganizadaApplication.java
+++ src/main/java/com/camara/organizada/OrganizadaApplication.java
@@ -1,10 +1,26 @@
 package com.camara.organizada;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class OrganizadaApplication {
+	
+    @Bean
+    public CacheManager cacheManager() {
+        // configure and return an implementation of Spring's CacheManager SPI
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("default")));
+        return cacheManager;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizadaApplication.class, args);
