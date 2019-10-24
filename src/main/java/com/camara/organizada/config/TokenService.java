6b17d060f6da8b05de651f6b6be38e064520e257
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-10-23 21:41:00 -0300

    changing token duration

--- src/main/java/com/camara/organizada/config/TokenService.java
+++ src/main/java/com/camara/organizada/config/TokenService.java
@@ -17,7 +17,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class TokenService {
 	
 	// EXPIRATION_TIME = 10 dias
-	static final long EXPIRATION_TIME = (5 * 60 * 60);
+	static final long EXPIRATION_TIME = 860_000_000;
 	static final String SECRET = "banana";
 	static final String TOKEN_PREFIX = "Bearer";
 	static final String HEADER_STRING = "Authorization";
